9c27831574ecc54d8ce8752abb0afd001211cd6d
Willam Yang Yang <wyangyan@ualberta.ca>
2025-10-31 14:55:37 -0600

    failing test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,13 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(edmonton);
 
+        // This will fail initially because hasCity() doesn't exist
+        assertTrue(list.hasCity(edmonton));
+    }
 }
